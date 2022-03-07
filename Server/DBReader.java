aad8cfb30f09ccb1d77f6c8bb5edb771511bc0aa
C2H8N2 <y.dusoleil@outlook.com>
2022-03-07 17:08:42 +0100

    Changed DB reading algorithm

--- Server/DBReader.java
+++ Server/DBReader.java
@@ -5,61 +5,68 @@ import data.structures.*;
 public class DBReader{
   public static void main(String[] args) {
     try(FileInputStream in = new FileInputStream("lichess_db_standard_rated_2013-01.pgn");){
-      StreamTokenizer st = new StreamTokenizer(in);
-      int token;
+      BufferedReader reader = new BufferedReader(new InputStreamReader(in));
       int cpt = 0;
+      int lineCpt = 0;
       do{
         Game tmp = new Game();
-        int start = st.lineno();
-        long startingByte = in.getChannel().position();
+        int startingLine = lineCpt;
+        String line = "";
+        int blankLineCpt = 0;
         do{
-          token = st.nextToken();
-          if(st.sval != null && st.sval.equals("Event")){
-            st.nextToken();
-            tmp.type = st.sval;
-          }else if(st.sval != null && st.sval.equals("Site")){
-            st.nextToken();
-            tmp.url = st.sval;
-          }else if(st.sval != null && st.sval.equals("White")){
-            st.nextToken();
-            tmp.whitePlayer = st.sval;
-          }else if(st.sval != null && st.sval.equals("Black")){
-            st.nextToken();
-            tmp.blackPlayer = st.sval;
-          }else if(st.sval != null && st.sval.equals("Result")){
-            st.nextToken();
-            tmp.result = st.sval;
-          }else if(st.sval != null && st.sval.equals("Opening")){
-            st.nextToken();
-            tmp.opening = st.sval;
+          line = reader.readLine();
+          if(line != null){
+            if(line.startsWith("[Event")){
+              tmp.type = line.substring(8,line.length()-2);
+            }
+            if(line.startsWith("[Site")){
+              tmp.url = line.substring(7,line.length()-2);
+            }
+            if(line.startsWith("[White ")){
+              tmp.whitePlayer = line.substring(8,line.length()-2);
+            }
+            if(line.startsWith("[Black ")){
+              tmp.blackPlayer = line.substring(8,line.length()-2);
+            }
+            if(line.startsWith("[Result")){
+              tmp.result = line.substring(9,line.length()-2);
+            }
+            if(line.startsWith("[Opening")){
+              tmp.opening = line.substring(10,line.length()-2);
+            }
+            if(line.equals("")){
+              blankLineCpt++;
+            }
           }
-        }while(st.lineno()-start < 18);
+          lineCpt++;
+        }while(blankLineCpt < 2);
         cpt++;
         System.out.println("=========== Game "+cpt+" - DONE ! ===========");
-        System.out.println(tmp.blackPlayer+" > "+"lichess_db_standard_rated_2013-01.pgn"+" > "+startingByte);
-        System.out.println(tmp.whitePlayer+" > "+"lichess_db_standard_rated_2013-01.pgn"+" > "+startingByte);
+        System.out.println(tmp.blackPlayer+" > "+"lichess_db_standard_rated_2013-01.pgn"+" > "+startingLine);
+        System.out.println(tmp.whitePlayer+" > "+"lichess_db_standard_rated_2013-01.pgn"+" > "+startingLine);
         if(new File("Players/"+tmp.blackPlayer+".dat").exists()){
-          Player blackPlayer = new Player("Players/"+tmp.blackPlayer+".dat");
-          blackPlayer.addGame("lichess_db_standard_rated_2013-01.pgn",startingByte);
-          blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
+          //   Player blackPlayer = new Player("Players/"+tmp.blackPlayer+".dat");
+          //   blackPlayer.addGame("lichess_db_standard_rated_2013-01.pgn",startingByte);
+          //   blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
         }else{
           Player blackPlayer = new Player();
           blackPlayer.pseudo = tmp.blackPlayer;
-          blackPlayer.addGame("lichess_db_standard_rated_2013-01.pgn",startingByte);
-          blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
+          blackPlayer.addGame("lichess_db_standard_rated_2013-01.pgn",(long)startingLine);
+          //   blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
         }
         if(new File("Players/"+tmp.whitePlayer+".dat").exists()){
-          Player whitePlayer = new Player("Players/"+tmp.whitePlayer+".dat");
-          whitePlayer.addGame("lichess_db_standard_rated_2013-01.pgn",startingByte);
-          whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
+          //   Player whitePlayer = new Player("Players/"+tmp.whitePlayer+".dat");
+          //   whitePlayer.addGame("lichess_db_standard_rated_2013-01.pgn",startingByte);
+          //   whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
         }else{
           Player whitePlayer = new Player();
           whitePlayer.pseudo = tmp.whitePlayer;
-          whitePlayer.addGame("lichess_db_standard_rated_2013-01.pgn",startingByte);
-          whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
+          whitePlayer.addGame("lichess_db_standard_rated_2013-01.pgn",(long)startingLine);
+          //   whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
         }
-      }while(token != st.TT_EOF);
+      }while(reader.ready());
       in.close();
+      System.out.println(cpt);
       System.out.println("====== FIN FICHIER ======");
     }catch (IOException e){
       e.printStackTrace();
