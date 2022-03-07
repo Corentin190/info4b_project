6986166a7ae8b4c905d0a67cba96b5637a4a7eaf
C2H8N2 <y.dusoleil@outlook.com>
2022-03-07 18:48:20 +0100

    Update DBReader.java

--- Server/DBReader.java
+++ Server/DBReader.java
@@ -8,6 +8,8 @@ public class DBReader{
       BufferedReader reader = new BufferedReader(new InputStreamReader(in));
       int cpt = 0;
       int lineCpt = 0;
+      ArrayList<String> playerNickname = new ArrayList<String>();
+      ArrayList<Player> players = new ArrayList<Player>();
       do{
         Game tmp = new Game();
         int startingLine = lineCpt;
@@ -44,27 +46,33 @@ public class DBReader{
         System.out.println("=========== Game "+cpt+" - DONE ! ===========");
         // System.out.println(tmp.blackPlayer+" > "+"lichess_db_standard_rated_2013-01.pgn"+" > "+startingLine);
         // System.out.println(tmp.whitePlayer+" > "+"lichess_db_standard_rated_2013-01.pgn"+" > "+startingLine);
-        if(new File("Players/"+tmp.blackPlayer+".dat").exists()){
-          Player blackPlayer = new Player("Players/"+tmp.blackPlayer+".dat");
+        if(playerNickname.contains(tmp.blackPlayer)){
+          Player blackPlayer = players.get(playerNickname.indexOf(tmp.blackPlayer));
           blackPlayer.addGame("lichess_db_standard_rated_2013-01.pgn",(long)startingLine);
-          blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
         }else{
           Player blackPlayer = new Player();
           blackPlayer.pseudo = tmp.blackPlayer;
           blackPlayer.addGame("lichess_db_standard_rated_2013-01.pgn",(long)startingLine);
-          blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
+          playerNickname.add(blackPlayer.pseudo);
+          players.add(blackPlayer);
+          //blackPlayer.savePlayer("Players/"+tmp.blackPlayer+".dat");
         }
-        if(new File("Players/"+tmp.whitePlayer+".dat").exists()){
-          Player whitePlayer = new Player("Players/"+tmp.whitePlayer+".dat");
+        if(playerNickname.contains(tmp.whitePlayer)){
+          Player whitePlayer = players.get(playerNickname.indexOf(tmp.whitePlayer));
           whitePlayer.addGame("lichess_db_standard_rated_2013-01.pgn",(long)startingLine);
-          whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
         }else{
           Player whitePlayer = new Player();
           whitePlayer.pseudo = tmp.whitePlayer;
           whitePlayer.addGame("lichess_db_standard_rated_2013-01.pgn",(long)startingLine);
-          whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
+          playerNickname.add(whitePlayer.pseudo);
+          players.add(whitePlayer);
+          //whitePlayer.savePlayer("Players/"+tmp.whitePlayer+".dat");
         }
       }while(reader.ready());
+      for(int i=0;i<players.size();i++){
+        System.out.println(players.get(i).pseudo);
+        players.get(i).savePlayer("Players/"+players.get(i).pseudo+".dat");
+      }
       in.close();
       System.out.println(cpt);
       System.out.println("====== FIN FICHIER ======");
