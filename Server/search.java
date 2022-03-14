155ce74c051cedf5c5cbd6276b9686555ecdad66
C2H8N2 <y.dusoleil@outlook.com>
2022-03-14 12:48:56 +0100

    Update search.java

--- Server/search.java
+++ Server/search.java
@@ -5,12 +5,12 @@ import data.searching.*;
 
 public class search{
   public static void main(String args[]){
-    PlayerGamesSearcher searcher = new PlayerGamesSearcher("ribarisah");
+    PlayerGamesSearcher searcher = new PlayerGamesSearcher("fil77");
     Game[] playerGames = searcher.load();
     if(playerGames!=null && playerGames.length>0){
       System.out.println(playerGames.length+" games found");
       for(int i=0;i<playerGames.length;i++){
-        System.out.println(playerGames[i].url);
+        System.out.println(playerGames[i].toString());
       }
     }else System.out.println("No game found for this nickname");
   }
