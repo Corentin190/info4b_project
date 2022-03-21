9cfa0b33308f1bd14833135bc007d2e230cfe86a
C2H8N2 <y.dusoleil@outlook.com>
2022-03-21 09:19:48 +0100

    Maintenant j'ai push donc fait pas le fou Coco

--- Server/data/searching/TopOpeningSearcher.java
+++ Server/data/searching/TopOpeningSearcher.java
@@ -4,37 +4,37 @@ import java.io.*;
 import java.util.*;
 
 public class TopOpeningSearcher{
-  private int nbTopOpening; 
-  public Hashtable<String,Integer> playersGamesList;
-  public String[] TopOpening;
+  private int nbTopOpening;
+  public Hashtable<String,Integer> openingList;
+  public String[] topOpening;
 
   public TopOpeningSearcher(int nbTopOpening){
     this.nbTopOpening = nbTopOpening;
-    this.playersGamesList = new Hashtable<String,Integer>();
-    this.mostActivePlayers = new String[nbTopOpening];
+    this.openingList = new Hashtable<String,Integer>();
+    this.topOpening = new String[nbTopOpening];
   }
 
   /*
-  Fonction initialisant les valeurs dans le tableau de mostActivePlayers.
+  Fonction initialisant les valeurs dans le tableau de topOpening.
   */
 
   private void sort(){
     List<String> keyList = new ArrayList<String>();
-    Enumeration<String> keys = playersGamesList.keys();
+    Enumeration<String> keys = openingList.keys();
     while(keys.hasMoreElements()){
       keyList.add(keys.nextElement());
     }
 
     //thanks https://stackoverflow.com/questions/5176771/sort-hashtable-by-values
-    ArrayList<Map.Entry<String, Integer>> l = new ArrayList(playersGamesList.entrySet());
+    ArrayList<Map.Entry<String, Integer>> l = new ArrayList(openingList.entrySet());
     Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
     public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
       return o2.getValue().compareTo(o1.getValue());
     }});
     //much thanks, such wow
 
-    for(int i=0;i<mostActivePlayers.length;i++){
-      mostActivePlayers[i] = l.get(i).getKey();
+    for(int i=0;i<topOpening.length;i++){
+      topOpening[i] = l.get(i).getKey();
     }
   }
 
@@ -43,11 +43,11 @@ public class TopOpeningSearcher{
       File root = new File("Src/");
 
       /*
-      Parcours de tous les fichiers et traitement des fichiers XXXXXX_player_data.dat
+      Parcours de tous les fichiers et traitement des fichiers XXXXXX_opening_data.dat
       */
 
       for(int i=0;i<root.list().length;i++){
-        if(root.list()[i].endsWith("_player_data.dat")){
+        if(root.list()[i].endsWith("_opening_data.dat")){
 
           /*
           Création du lecteur pour la lecture du fichier et recherche du joueur.
@@ -62,24 +62,23 @@ public class TopOpeningSearcher{
           /*
           Extraction de tous les joueurs et stockage dans une hashtable
           */
-          //à faire, demande à Yaon
-          // while(reader.ready()){
-          //   line = reader.readLine();
-          //   if(line.startsWith("[Opening ")){
-          //     String Opening = line.substring(10,line.length()-2);
-          //     if(this.playersGamesList.containsKey(Opening)){
-          //       do{
-          //         line = reader.readLine();
-          //       }while(!line.startsWith("[NumberOfGame "));
-          //       this.playersGamesList.put(playerNickname,this.playersGamesList.get(playerNickname)+Integer.parseInt(line.substring(14,line.length()-1)));
-          //     }else{
-          //       do{
-          //         line = reader.readLine();
-          //       }while(!line.startsWith("[NumberOfGame "));
-          //       this.playersGamesList.put(playerNickname,Integer.parseInt(line.substring(14,line.length()-1)));
-          //     }
-          //   }
-          // }
+          while(reader.ready()){
+            line = reader.readLine();
+            if(line.startsWith("[Opening ")){
+              String opening = line.substring(10,line.length()-2);
+              if(this.openingList.containsKey(opening)){
+                do{
+                  line = reader.readLine();
+                }while(!line.startsWith("[NumberOfOccurence "));
+                this.openingList.put(opening,this.openingList.get(opening)+Integer.parseInt(line.substring(20,line.length()-2)));
+              }else{
+                do{
+                  line = reader.readLine();
+                }while(!line.startsWith("[NumberOfOccurence "));
+                this.openingList.put(opening,Integer.parseInt(line.substring(20,line.length()-2)));
+              }
+            }
+          }
           reader.close();
           in.close();
         }
---
+++ Server/topOpening.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import data.structures.*;
+import data.searching.*;
+
+public class topOpening{
+  public static void main(String args[]){
+    TopOpeningSearcher searcher = new TopOpeningSearcher(10);
+    searcher.load();
+    for(int i=0;i<searcher.topOpening.length;i++){
+      System.out.println(i+". "+searcher.topOpening[i]+" - "+searcher.openingList.get(searcher.topOpening[i]));
+    }
+  }
+}
