9e9b2a559ac2bc4710ba8e900d83a5ed33c89feb
C2H8N2 <y.dusoleil@outlook.com>
2022-03-21 09:25:42 +0100

    Update topOpening.java

--- Server/topOpening.java
+++ Server/topOpening.java
@@ -5,7 +5,7 @@ import data.searching.*;
 
 public class topOpening{
   public static void main(String args[]){
-    TopOpeningSearcher searcher = new TopOpeningSearcher(10);
+    TopOpeningSearcher searcher = new TopOpeningSearcher(1000);
     searcher.load();
     for(int i=0;i<searcher.topOpening.length;i++){
       System.out.println(i+". "+searcher.topOpening[i]+" - "+searcher.openingList.get(searcher.topOpening[i]));
