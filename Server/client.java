db692ae1dcc16f7a5752955fbfde43419090a59f
Corentin190 <corentin.diehl@gmail.com>
2022-03-22 12:11:03 +0100

    Create client.java

---
+++ Server/client.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+
+public class client {
+    public static void main(String args[]) {
+        Socket clientSocket = new Socket();
+        InputStream input;
+        InputStreamReader reader;
+        OutputStream output;
+        try {
+            clientSocket.connect(new InetSocketAddress("192.168.99.185",1085));
+            output = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+            writer.println("This is a message sent to the server");
+            clientSocket.close();
+        }catch (IOException e){
+        e.printStackTrace();
+    }
+        
+    }
+}
