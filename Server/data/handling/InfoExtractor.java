d58d52a18ded01f24168e49deaba34413460b47a
C2H8N2 <y.dusoleil@outlook.com>
2022-04-07 15:50:07 +0200

    Update InfoExtractor.java

--- Server/data/handling/InfoExtractor.java
+++ Server/data/handling/InfoExtractor.java
@@ -7,6 +7,7 @@ import java.util.*;
 public class InfoExtractor extends Thread{
   private GameBuffer buffer;
   private CommonRessources ressources;
+  private final int BUFFER_POP_AMOUNT;
 
   private Hashtable<String,Integer> openingHashtable;
   private Hashtable<String,ArrayList<Long>> playersHashtable;
@@ -15,11 +16,23 @@ public class InfoExtractor extends Thread{
   public InfoExtractor(GameBuffer buffer, CommonRessources ressources){
     this.buffer = buffer;
     this.ressources = ressources;
+    /*
+    Same issue as the producer thread. Because too much synchronized access to the buffer greatly slows the reading speed we have to limit it's pop frequency.
+    This constant is the number of games popped at once by this specific thread each time it accesses the buffer.
+    */
+    this.BUFFER_POP_AMOUNT = 1000;
+    /*
+    Each consumer thread has it's own hashtables. This increases RAM usage but greatly improves reading speed.
+    */
     this.openingHashtable = new Hashtable<String,Integer>();
     this.playersHashtable = new Hashtable<String,ArrayList<Long>>();
     this.urlHashtable = new Hashtable<String,Long>();
   }
 
+  /*
+  This function extract the players related Data from the game passed in arguments.
+  If the player already exists in the Hashtable it just add the game to it's key. Else it creates a new key wich is the player's name.
+  */
   public synchronized void extractPlayerData(Game game){
     if(this.playersHashtable.containsKey(game.blackPlayer)) {
       this.playersHashtable.get(game.blackPlayer).add(game.startingByte);
@@ -49,18 +62,18 @@ public class InfoExtractor extends Thread{
 
   public void run(){
     try{
-      long readGameStart = System.currentTimeMillis();
-      long readTime = 0;
-      ArrayList<String> internalBuffer = buffer.popMultiple(1000);
-      while(internalBuffer.size()>0){
-        while(internalBuffer.size()>0){
+      ArrayList<String> internalBuffer = buffer.popMultiple(this.BUFFER_POP_AMOUNT);
+      while(internalBuffer.size()>0){     //In this case, if the internalBuffer is empty it means that the popMultiple() function hasn't return any games.
+        while(internalBuffer.size()>0){   //In this case, if the internalBuffer is empty it means that the consumer thread has processed all it's games.
+          /*
+          Exctract informations from the first game contained in it's internalBuffer.
+          */
           BufferedReader reader = new BufferedReader(new StringReader(internalBuffer.get(0)));
           String line = "";
           Game tmp = new Game();
           boolean sameGame=false;
           do{
             line = reader.readLine();
-
             if(line != null){
               if(line.startsWith("[Byte")){
                 tmp.startingByte = Long.parseLong(line.substring(6,line.length()-1));
@@ -88,17 +101,17 @@ public class InfoExtractor extends Thread{
               }
             }
           }while(line!=null);
-          if(sameGame)System.out.println(tmp.toString());
-          sameGame = false;
+          /*
+          Increment the shared game counter in the CommonRessources and extracts player's data, opening data and url related data to it's internal Hastable
+          */
           ressources.incrGame();
           this.extractPlayerData(tmp);
           this.extractOpeningIteration(tmp);
           this.extractUrl(tmp);
-          readTime = System.currentTimeMillis()-readGameStart;
           readGameStart = System.currentTimeMillis();
           internalBuffer.remove(0);
         }
-        internalBuffer = buffer.popMultiple(1000);
+        internalBuffer = buffer.popMultiple(this.BUFFER_POP_AMOUNT);
       }
       ressources.mergePlayerData(this.playersHashtable);
       ressources.mergeOpeningData(this.openingHashtable);
