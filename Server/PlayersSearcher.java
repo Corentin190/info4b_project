fe9250b4113fcca22ed94c59c4527e19a24333d3
Corentin190 <corentin.diehl@gmail.com>
2022-04-07 15:11:03 +0200

    Update PlayersSearcher.java

--- Server/PlayersSearcher.java
+++ Server/PlayersSearcher.java
@@ -2,9 +2,15 @@ import java.io.*;
 import java.util.*;
 
 
+/*
+This function create a file called 'playersData.dat' that contains the name of all players with their number of game.
+They are sorted from the most active to the less.
+The most active who is the one with the higher count of game.
+*/
 
 public class PlayersSearcher{
   public static void main(String[] args) {
+    //Creation of an arraylist wich contains all the data files '*_player_data.dat', in alphabetic order.
     File folder = new File("Src/");
     ArrayList<String> fileFolder = new ArrayList<>();
     for(int i=0;i<folder.list().length;i++){
@@ -12,8 +18,8 @@ public class PlayersSearcher{
         fileFolder.add(folder.list()[i]);
       }
     }
+    fileFolder.sort(String::compareToIgnoreCase);
     try{
-      fileFolder.sort(String::compareToIgnoreCase);
       Hashtable<String,Integer> players = new Hashtable<String, Integer>();
       String dataFile="";
       FileInputStream in;
@@ -26,6 +32,10 @@ public class PlayersSearcher{
         System.out.println("Dealing with "+dataFile+"...");
         in = new FileInputStream("Src/"+dataFile);
         reader = new BufferedReader(new InputStreamReader(in));
+        /*
+        Reading of each line of the files, if the line begins with '[Pseudo', then the player is added to an Hashtable and his number of game is added too.
+        If the player is already in the Hashtable, his number of game is updated.
+        */
         do{
           line=reader.readLine();
           if(line.startsWith("[Pseudo")){
@@ -43,7 +53,9 @@ public class PlayersSearcher{
         }while(reader.ready());
         System.out.println(dataFile+" done.");
       }
-      File outputPlayer = new File("Src/playersData.dat");
+      /*
+          Sort the Hashtable from the most to the less active.
+      */
       ArrayList<Map.Entry<String, Integer>> l = new ArrayList(players.entrySet());
       Collections.sort(l, new Comparator<Map.Entry<String, Integer>>(){
         public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2){
@@ -53,6 +65,10 @@ public class PlayersSearcher{
       for(int i=0;i<playersTab.length;i++){
         playersTab[i] = l.get(i).getKey();
       }
+      /*
+      Writing of the Hashtable in file 'playersData.dat', in order to be read easily.
+      */
+      File outputPlayer = new File("Src/playersData.dat");
       FileOutputStream out = new FileOutputStream(outputPlayer);
       OutputStreamWriter writer = new OutputStreamWriter(out);
       Enumeration<String> keys = players.keys();
